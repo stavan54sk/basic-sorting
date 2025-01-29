a7df34a0d0e721b3f80cfa235a727181bfb8c3b1
stavan54sk <stavan54sk@gmail.com>
2025-01-29 23:32:04 +0530

    completed 2 sorting techniques

--- src/main/java/com/sorting/App.java
+++ src/main/java/com/sorting/App.java
@@ -1,14 +1,12 @@
 package com.sorting;
 
-import java.util.Iterator;
-
 /**
  * Hello world!
  *
  */
 public class App {
 
-	void bubblesort(int[] a, int n) {
+	void bubbleSort(int[] a, int n) {
 		for (int i = 0; i < n; i++) {
 			boolean isSorted = true;
 			for (int j = 0; j < n - i - 1; j++) {
@@ -27,10 +25,41 @@ public class App {
 
 	}
 
+	void selectionSort(int[] a, int n) {
+		for (int i = 0; i < n; i++) {
+			int minIdex = i;
+			for (int j = i + 1; j < a.length; j++) {
+				if (a[j] < a[minIdex]) {
+					int temp = a[minIdex];
+					a[minIdex] = a[j];
+					a[j] = temp;
+
+				}
+
+			}
+
+		}
+
+	}
+
+	void insertionSort(int[] a, int n) {
+		for (int i = 1; i < n; i++) {
+			int c = a[i];
+			int p = i - 1;
+			while (p >= 0 && a[p] > c) {
+				a[p + 1] = a[p];
+				p--;
+
+			}
+			a[p + 1] = c;
+		}
+
+	}
+
 	public static void main(String[] args) {
 
 		int[] a = { 5, 6, 7, 2, 9, 8 };
-		new App().bubblesort(a, 6);
+		new App().insertionSort(a, 6);
 
 		for (int i = 0; i < a.length; i++) {
 			System.out.print(a[i]);
